94c1afdf5b50a4b5c33411d753b8fe3262d8b6cb
Matthew <espi5730@pacificu.edu>
2024-06-24 23:27:59 +0000

    main while loop set up, gather user input

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from openai import OpenAI
+import os
+
+QUIT = 'Q'
+
+user_input = ""
+
+current_word = 'mors'
+
+while user_input != QUIT:
+
+    #run another question
+
+    try:
+        user_input = str(input(f'Define the word {current_word}: '))
+
+    except:
+        #redundant
+        #user_input = input(f'test: ')
+        #user_input = input(f'Define the word {current_word}: ')
 
 
+
+#Request URL
+#https://www.stands4.com/services/v2/defs.php
