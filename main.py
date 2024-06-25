d70c5391dbf3e78095ed10b8b49a4bdfb64a043c
Matthew <espi5730@pacificu.edu>
2024-06-25 22:25:29 +0000

    brice api changes and matthew definition api changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import os
 import requests
 import random
+from words import word_list
 
 # consts
 QUIT = 'Q'
@@ -17,7 +18,7 @@ url = ''
 # functions
 
 
-def useChatGPT(definition, word_to_define):
+def useChatGPT(user_definition, word_to_define, actual_definition):
 
     my_api_key = os.getenv('OPENAI_KEY')
 
@@ -29,38 +30,43 @@ def useChatGPT(definition, word_to_define):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are an english professor and" +
-                " can identify words based on their definitions."},
-            {"role": "user", "content": "Is the following phrase a good" +
-                " definition of the word " + word_to_define + " :" +
-                definition}
+                " can identify whether a phrase is a good is "
+				+ "definition based on a provided definition."},
+            {"role": "user", "content": "Is the following definition: " + user_definition +
+				" a good definition of the word " + word_to_define +
+                " ,based on the following definition: " + actual_definition
+				+ " and give the definition a grade using the A-F grading scale."
+                }
             ]
     )
     print(completion.choices[0].message.content)
 
 
-def getNewWord(word_list):
+def getNewWord(word_lst):
     # fill in
-    """
-    Purpose: to get a random new word
-    1) select a random word from word_list
-    2) delete selected word from word_list
-    3) return the selected word
-    """
+	new_word = random.choice(word_lst)
+	word_lst.remove(new_word)
+	return new_word
 
-# PEP warning being difficult
-url = "https://www.stands4.com/services/v2/defs.php?uid=" + uid + "&tokenid=" + tokenid + "&word=" + current_word + "&format=json"
+def getDefintion(word, uid, tokenid):
+    # PEP warning being difficult
+    url = "https://www.stands4.com/services/v2/defs.php?uid=" + uid + "&tokenid=" + tokenid + "&word=" + word + "&format=json"
 
-response = requests.get(url, headers={'User-Agent': 'curl/7.81.0'})
+    response = requests.get(url, headers={'User-Agent': 'curl/7.81.0'})
 
+    definition = response.json()
+    result = definition['result'][0]['definition']
+    return result
 
-if response.status_code == 200:
-    print(response.json())
-else:
-    print(response.status_code)
+print(getDefintion(current_word, uid, tokenid))
+
 
 while user_input != QUIT:
 
-    # run another question
+    # run a question
+    current_word = getNewWord(word_list)
+
+	# get user definition
 
     try:
         # PEP warning being difficult
@@ -70,4 +76,9 @@ while user_input != QUIT:
         # Error Catch
         print("how did you mess this up")
 
-    # useChatGPT(user_input, current_word)
+	# get actual definition
+
+    current_definition = getDefintion(current_word, uid, tokenid)
+
+
+    useChatGPT(user_input, current_word, current_definition)
