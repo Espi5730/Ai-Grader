6879383094184f7cdc44b72f3a4d54a4149e8d8e
Matthew <espi5730@pacificu.edu>
2024-06-26 04:03:04 +0000

    PEP updates

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from words import word_list
 
 # consts
 QUIT = 'Q'
-BASE_URL = 'https://www.stands4.com/services/v2/defs.php'
+BASE_URL = 'https://www.stands4.com/services/v2/defs.php?uid='
+FORMAT = '&format=json'
+QUIT_PROMPT = '(or press Q to quit)'
+PROMPT = 'Define the word'
 
 # vars
 uid = '12640'
@@ -31,13 +34,14 @@ def useChatGPT(user_definition, word_to_define, actual_definition):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are an english professor and" +
-                " can identify whether a phrase is a good is "
-				+ "definition based on a provided definition."},
-            {"role": "user", "content": "Is the following definition: " + user_definition +
-				" a good definition of the word " + word_to_define +
-                " ,based on the following definition: " + actual_definition
-				+ " and give the definition a grade using the A-F grading scale."
-                }
+                " can identify whether a phrase is a good is " +
+                "definition based on a provided definition."},
+            {"role": "user", "content": "Is the following definition: " +
+                user_definition +
+                " a good definition of the word " + word_to_define +
+                " ,based on the following definition: " + actual_definition +
+                " and give the definition a grade" +
+                " using the A-F grading scale."}
             ]
     )
     print(completion.choices[0].message.content)
@@ -45,13 +49,14 @@ def useChatGPT(user_definition, word_to_define, actual_definition):
 
 def getNewWord(word_lst):
     # fill in
-	new_word = random.choice(word_lst)
-	word_lst.remove(new_word)
-	return new_word
+    new_word = random.choice(word_lst)
+    word_lst.remove(new_word)
+    return new_word
+
 
 def getDefintion(word, uid, tokenid):
     # PEP warning being difficult
-    url = "https://www.stands4.com/services/v2/defs.php?uid=" + uid + "&tokenid=" + tokenid + "&word=" + word + "&format=json"
+    url = BASE_URL + uid + "&tokenid=" + tokenid + "&word=" + word + FORMAT
 
     response = requests.get(url, headers={'User-Agent': 'curl/7.81.0'})
 
@@ -59,45 +64,36 @@ def getDefintion(word, uid, tokenid):
     result = definition['result'][0]['definition']
     return result
 
-# print(getDefintion(current_word, uid, tokenid))
-
 
 while user_input != QUIT:
-	
-	
-	
+
     # run a question
     current_word = getNewWord(word_list)
 
-	# get user definition
+    # get user definition
 
     try:
         # PEP warning being difficult
         if count == 0:
-            user_input = str(input(f'Define the word {current_word} (or press Q to quit): '))
+            user_input = str(input(f'{PROMPT} {current_word} {QUIT_PROMPT}: '))
             count += 1
         else:
             print("\n")
-            user_input = str(input(f'Define the word {current_word} (or press Q to quit): '))
+            user_input = str(input(f'{PROMPT} {current_word} {QUIT_PROMPT}: '))
 
     except:
         # Error Catch
         print("how did you mess this up")
-	
+
     # gives space between loop iterations
     print("\n")
 
-	# prevent chatgpt from using "Q" as an input
+    # prevent chatgpt from using "Q" as an input
     if user_input == QUIT:
         break
-	
-	# get actual definition
+
+    # get actual definition
 
     current_definition = getDefintion(current_word, uid, tokenid)
 
-
     useChatGPT(user_input, current_word, current_definition)
-
-	
-
-    
