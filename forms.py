e6336942f0452bf139dae765faac33327f27543f
Matthew Espinoza <112516041+Espi5730@users.noreply.github.com>
2024-07-12 06:58:33 +0000

    style adherance

--- forms.py
+++ forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, HiddenField
+from wtforms import StringField, SubmitField, BooleanField, HiddenField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+
 
 class userPrompt(FlaskForm):
     word = HiddenField('Word')
 
     definition = StringField('Definition',
-                           validators=[DataRequired(), Length(min=3)])
-    
+                             validators=[DataRequired(), Length(min=3)])
+
     submit = SubmitField('Submit')
 
     def getDefintion(self):
--- main.py
+++ main.py
@@ -10,8 +10,6 @@ import requests
 import random
 import sqlite3
 import pandas as pd
-#random extra text
-#more random extra text
 
 # consts
 QUIT = 'Q'
@@ -29,16 +27,17 @@ current_grade = ''
 url = ''
 count = 0
 
+
 # functions
-def addDatabase(chat_gpt_response,word):
-        c.execute('''
-        INSERT INTO results (word,grade)
-        VALUES (?, ?)
-    ''', (word,chat_gpt_response[1])
+def addDatabase(chat_gpt_response, word):
+    c.execute('''
+    INSERT INTO results (word, grade)
+    VALUES (?, ?)
+    ''', (word, chat_gpt_response[1])
     )
 
+
 def useChatGPT(user_definition, word_to_define, actual_definition):
-    
     # figure out how to replace user_definition with flask requests
 
     my_api_key = os.getenv('OPENAI_KEY')
@@ -54,17 +53,18 @@ def useChatGPT(user_definition, word_to_define, actual_definition):
              " can identify whether a phrase is a good is " +
              "definition based on a provided definition."},
             {"role": "user", "content": "Is the following definition: " +
-             user_definition +
-             " a good definition of the word " + word_to_define +
-             " ,based on the following definition: " + actual_definition +
+             user_definition + " a good definition of the word " +
+             word_to_define + " ,based on the following definition: " +
+             actual_definition +
              " and give the definition a grade using the A-F grading scale" +
              " in the format of Grade: *Grade* on a new Line." +
-             " Give the explanation first, then display the grade after the explanation. Don't display the grade first before the explanation"}
+             " Give the explanation first, then display" +
+             " the grade after the explanation." +
+             " Don't display the grade first before the explanation"}
             ]
     )
 
     output = completion.choices[0].message.content
-    #print(output)
 
     tmp = output[-2:]
     grade = ""
@@ -93,138 +93,57 @@ def getDefintion(word, uid, tokenid):
     result = definition['result'][0]['definition']
     return result
 
-#secret key
+
 key = secrets.token_hex(16)
 # setting up the database
-conn =  sqlite3.connect('gradebook.db', check_same_thread=False)
+conn = sqlite3.connect('gradebook.db', check_same_thread=False)
 c = conn.cursor()
 c.execute('''
     CREATE TABLE IF NOT EXISTS results (
     questionNumber INTEGER PRIMARY KEY,
     word TEXT NOT NULL,
     Grade CHAR
-    ) 
+    )
 ''')
 
 app = Flask(__name__)
-proxied = FlaskBehindProxy(app) 
+proxied = FlaskBehindProxy(app)
 
-app.config['SECRET_KEY'] =  key
+app.config['SECRET_KEY'] = key
+
 
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
-    form = userPrompt() 
+    form = userPrompt()
 
     if request.method == 'POST' and form.validate_on_submit():
         user_definition = form.getDefintion()
         word = form.word.data
-        output = useChatGPT(str(user_definition), word, getDefintion(word, uid, tokenid))
+
+        output = useChatGPT(str(user_definition),
+                            word, getDefintion(word, uid, tokenid))
+
         form.word.data = getNewWord(word_list)
-        addDatabase(output,word)
-        return render_template('home.html', form=form, message=output[0], grade=output[1], word=form.word.data)
+        addDatabase(output, word)
+        return render_template('home.html', form=form,
+                               message=output[0], grade=output[1],
+                               word=form.word.data)
 
     form.word.data = getNewWord(word_list)
     return render_template('home.html', form=form, word=form.word.data)
-    
+
+
 @app.route("/report")
 def grade_page():
-    tmp=c.execute('SELECT * FROM results')
+    tmp = c.execute('SELECT * FROM results')
     html = tmp
-    return render_template('report.html',table=html)
+    return render_template('report.html', table=html)
+
 
 @app.route("/about")
 def about_page():
     return render_template('about.html')
 
-# @app.route("/report")
-# def displayData():
-#     tmp=c.execute('SELECT * FROM results')
-#     df=pd.DataFrame(tmp.fetchall())
-#     df.columns=tmp.keys()
-#     html = df.to_html()
-#     return render_template('report.html',table=html)
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
-
-"""
-# setting up the database
-conn = sqlite3.connect('gradebook.db')
-c = conn.cursor()
-c.execute('''
-    CREATE TABLE IF NOT EXISTS results (
-    questionNumber INTEGER PRIMARY KEY,
-    word TEXT NOT NULL,
-    Grade CHAR
-    ) 
-''')
-
-
-
-
-while user_input != QUIT:
-
-    # run a question
-    current_word = getNewWord(word_list)
-
-    # get user definition
-
-    if count == 0:
-        user_input = str(input(f'{PROMPT} {current_word} {QUIT_PROMPT}: '))
-        count += 1
-    else:
-        print("\n")
-        user_input = str(input(f'{PROMPT} {current_word} {QUIT_PROMPT}: '))
-        count += 1
-
-    # gives space between loop iterations
-    print("\n")
-
-    # prevent chatgpt from using "Q" as an input
-    if user_input == QUIT:
-        break
-
-    # get actual definition
-
-    current_definition = getDefintion(current_word, uid, tokenid)
-
-    # send to chatGPT
-
-    grade_response = useChatGPT(userPrompt, current_word, current_definition)
-
-    # append everything to the database
-    c.execute('''
-        INSERT INTO results (word, actualDef, grade )
-        VALUES (?, ?, ?)
-    ''', (grade_response[1], current_definition, current_grade)
-    )
-
-
-c.execute(
-    'SELECT * FROM results'
-)
-print(f"{'Question':<15}{'Word':<15}{'Grade':<10}")
-data = c.fetchall()
-for rows in data:
-    num = str(rows[0])
-    word = str(rows[1])
-    definition = str(rows[2])
-    grade = str(rows[3])
-    print(f"{num:<15}{word:<15}{grade:<10}")
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# this is why the code will break most likely
-conn.commit()
-conn.close()
-
-"""
